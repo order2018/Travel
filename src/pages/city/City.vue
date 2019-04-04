cbd59ed3058e0446850436d87331a9a59225541c
403228075@qq.com <403228075@qq.com>
2019-04-04 15:35:48 +0800

    项目城市选择页-路由配置

---
+++ src/pages/city/City.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="city">
+      <city-header></city-header>
+  </div>
+</template>
+
+<script>
+import CityHeader from './components/Header'
+export default {
+  name: 'City',
+  data () {
+    return {
+
+    }
+  },
+  components: {
+      CityHeader
+  },
+  methods: {
+
+  },
+  mounted() {
+
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
