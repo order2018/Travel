96d2d8a5d1902d260090de5ec75610ea9f4c057c
403228075@qq.com <403228075@qq.com>
2019-04-04 15:59:09 +0800

    搜索款布局

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="city">
       <city-header></city-header>
+      <city-search></city-search>
   </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
+import CitySearch from './components/Search'
 export default {
   name: 'City',
   data () {
@@ -14,7 +16,8 @@ export default {
     }
   },
   components: {
-      CityHeader
+      CityHeader,
+      CitySearch
   },
   methods: {
 
